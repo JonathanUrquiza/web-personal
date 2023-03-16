1c204b3a02a53e65551047a38d5daa0989221808
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-03-15 23:42:40 -0300

    to start

---
+++ js/servicios.js
@@ -0,0 +1,8 @@
+const basicoEstatico = document.getElementById('basicoEstatico'), medioEstatico = document.getElementById('medioEstatico'), basicoDinamico = document.getElementById('basicoDinamico'), medioDinamico = document.getElementById('medioDinamico');
+console.log(basicoEstatico);
+console.log(medioEstatico);
+console.log(basicoDinamico);
+console.log(medioDinamico);
+
+let mDinamico = medioDinamico.innerText;
+console.log(mDinamico);
