20370c384b3a7a3c0dde0c056f93b8c8e6a01046
Jonathan Urquiza <jurquiza86@hotmail.com>
2024-02-29 18:14:03 -0300

    form

---
+++ index.js
@@ -0,0 +1,7 @@
+document.title = 'Software Dev.';//all HTML
+import { date } from "./js/service/date.js";date()
+import {  } from "./js/form.js"
+
+
+
+
--- js/service/date.js
+++ js/service/date.js
@@ -1,7 +1,7 @@
 //class DATE del index
 
 export const date = () => {
-    const toDay = document.getElementById('toDay'), day = document.getElementById('day');
+    const toDay = document.getElementById('toDay');
     const fecha = new Date()
     const year = toDay.innerText = fecha.getFullYear();//Actualizaciones del año en el copy.
     return year
